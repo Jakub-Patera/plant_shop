491dddec0d82d6ebe1935b9867d0ddf8bd9781c7
Jakub Patera <patera.jakub686@gmail.com>
2023-10-01 13:54:17 +0200

    Init - plant shop Vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="flex justify-center items-center flex-row">
+        
+        <ul class="flex flex-row space-x-6">
+          <a><li>Home</li></a>
+          <a><li>About</li></a>
+          <a><li>Products</li></a>
+          <a><li>FAQs</li></a>
+          <a><li>Contact Us</li></a>
+      </ul>
+    </section>
+</template>
